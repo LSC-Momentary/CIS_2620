0889727c1379626b66591ad13198f9d421177908
LSC-Momentary <33061578+LSC-Momentary@users.noreply.github.com>
2019-01-24 22:34:41 -0600

    Add files via upload

---
+++ Greenville_Motto/Greenville_Motto/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Greenville_Motto
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            WriteLine("The stars shine in Greenville.");
+        }
+    }
+}
